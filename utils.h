256f106e6bdef7112f37bacc9544575383916dce
Zach Sitler <zachsitler@yahoo.com>
2014-09-11 17:29:27 -0700

    Remove duplicate headers because oops.

--- utils.h
+++ utils.h
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <stdlib.h
-#include <string.h>
-#include <unistd.h> 
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h> 
 
 #ifndef HEADER_FILE
 #define HEADER_FILE
